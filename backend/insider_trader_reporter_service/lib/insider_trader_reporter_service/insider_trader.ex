fa1d708d806b403110ddf4c331de82811b20e234
Mateus Bernardi <mateusgabrielbb@gmail.com>
2024-02-28 09:12:25 -0300

    add workaround for reports with more than one reporter data

--- backend/insider_trader_reporter_service/lib/insider_trader_reporter_service/insider_trader.ex
+++ backend/insider_trader_reporter_service/lib/insider_trader_reporter_service/insider_trader.ex
@@ -15,6 +15,7 @@ defmodule InsiderTraderReporterService.InsiderTrader do
   def get_insider_trader_data(company_form_data) do
     insider_data = company_form_data
     |> Map.get("reportingOwner", %{})
+    |> check_and_adjust_collection_type()
     insider_name = insider_data
     |> Map.get("reportingOwnerId", %{})
     |> Map.get("rptOwnerName", "")
@@ -22,5 +23,12 @@ defmodule InsiderTraderReporterService.InsiderTrader do
     |> Map.get("reportingOwnerRelationship", %{})
     |> Map.get("officerTitle", "")
     new(insider_name, insider_title)
+  end
+
+  defp check_and_adjust_collection_type(insider_data) do
+    case is_list(insider_data) do
+      true -> hd(insider_data)
+      false -> insider_data
+    end
   end
 end
